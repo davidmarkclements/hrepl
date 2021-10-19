c9b1b122b09dd1118bc2259d920407aaef5dec53
David Mark Clements <david.mark.clements@gmail.com>
2021-10-19 19:56:33 +0200

    print out api, warn on default export

--- cmd.js
+++ cmd.js
@@ -4,9 +4,15 @@ import { start } from 'repl'
 
 const file = await realpath(process.argv.splice(2, 1).pop())
 process.argv[1] = file
-const api = await import(file)
-delete api.default
+const api = {...await import(file)}
+
+if (api.default) {
+  process.emitWarning('Default export cannot be exposed in hrepl (default is a reserved word)\n')
+  delete api.default
+}
 
 Object.assign(global, api)
+
+console.log('API:', Object.entries(api).map(([k, v]) => `${k} (${Array.isArray(v) ? 'array' : typeof v})`).join(', '))
 
 start({ ignoreUndefined: true })
